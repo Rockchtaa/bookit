61a28f40c042e6a7070c7f7ba0162021414dc9e1
Yassine <yassineelbrahmi11@gmail.com>
2025-04-20 23:17:45 +0200

    feat: implement async data fetching for available rooms and handle error redirection

--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,22 @@
-import data from "../data/rooms.json";
+import { redirect } from 'next/navigation';
 import CardContent from "@/Components/CardContent";
 import Heading from "@/Components/Heading";
+import getAllRooms from "./actions/getAllRooms";
 
-export default function Home() {
-  return ( <>
+export default async function Home() {
+  const data = await getAllRooms();
+
+  if (!data) {
+    redirect('/error');
+  }
+
+  return (
+    <>
       <Heading title="Available Rooms" />
-      {data.length > 0 ? ( 
+      {data.length > 0 ? (
         data.map((room) => <CardContent room={room} key={room.$id} />)
       ) : (
-          <h2>No rooms available</h2>
+        <h2>No rooms available</h2>
       )}
     </>
   );
