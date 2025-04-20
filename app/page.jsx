a590c0db05f856103ad16f9ed526e7675127028a
Yassine <yassineelbrahmi11@gmail.com>
2025-04-20 20:02:54 +0200

    feat: update Home component to display room cards with headings

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,12 @@
 import data from "../data/rooms.json";
+import CardContent from "@/Components/CardContent";
+import Heading from "@/Components/Heading";
 
 export default function Home() {
   return ( <>
+      <Heading title="Available Rooms" />
       {data.length > 0 ? ( 
-        data.map((room) => <h2>{room.name}</h2> )
+        data.map((room) => <CardContent room={room} key={room.$id} />)
       ) : (
           <h2>No rooms available</h2>
       )}
