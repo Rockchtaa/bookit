700461b6f055fade9bc346477ee4e907613b1d10
Yassine <yassineelbrahmi11@gmail.com>
2025-04-21 14:43:15 +0200

    feat: add ToastContainer for displaying notifications in layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,6 +4,9 @@ import "../assets/styles/globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-inter",
@@ -23,6 +26,7 @@ export default function RootLayout({ children }) {
           {children}
         </main>
         <Footer />
+        <ToastContainer />
       </body>
     </html>
   );
