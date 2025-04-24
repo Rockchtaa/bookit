b85f3d551c25ed7d50ebfff08ab8d4ffa8d97834
Yassine <yassineelbrahmi11@gmail.com>
2025-04-24 13:03:57 +0200

    feat: refactor createSession function to improve session handling and error management

--- app/actions/createSession.js
+++ app/actions/createSession.js
@@ -1,5 +1,6 @@
-'use server';
-
+"use server";
+import { createAdminClient } from "@/config/appwrite";
+import { cookies } from "next/headers";
 async function createSession(prevState, formData) {
   const email = formData.get("email");
   const password = formData.get("password");
@@ -8,10 +9,23 @@ async function createSession(prevState, formData) {
     return { error: "Email and password are required." };
   }
 
+  const { account } = await createAdminClient();
+  try {
+    const session = await account.createEmailPasswordSession(email, password);
+    console.log("Session created successfully:", session);
+    cookies().set("appwrite_session", session.secret, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+      expires: new Date(Date.now() + 60 * 60 * 1000),
+      path: "/",
+    });
+    return { success: true };
+  } catch (error) {
+    console.error("Error creating session:", error);
+    return { error: "Failed to create session. Please check your credentials." };
+  }
 
-
-  // If successful:
-  return { success: true };
 }
 
 export default createSession;
