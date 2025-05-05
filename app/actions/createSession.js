f1a9d34d2c368e715b0690549004473a21962ac9
Yassine <yassineelbrahmi11@gmail.com>
2025-05-05 21:47:04 +0200

    fix: improve cookie handling in createSession function

--- app/actions/createSession.js
+++ app/actions/createSession.js
@@ -1,6 +1,8 @@
 "use server";
+
 import { createAdminClient } from "@/config/appwrite";
 import { cookies } from "next/headers";
+
 async function createSession(prevState, formData) {
   const email = formData.get("email");
   const password = formData.get("password");
@@ -13,19 +15,21 @@ async function createSession(prevState, formData) {
   try {
     const session = await account.createEmailPasswordSession(email, password);
     console.log("Session created successfully:", session);
-    cookies().set("appwrite_session", session.secret, {
+
+    const cookieStore = await cookies();
+    cookieStore.set("appwrite_session", session.secret, {
       httpOnly: true,
       secure: true,
       sameSite: "strict",
       expires: new Date(Date.now() + 60 * 60 * 1000),
       path: "/",
     });
+
     return { success: true };
   } catch (error) {
     console.error("Error creating session:", error);
     return { error: "Failed to create session. Please check your credentials." };
   }
-
 }
 
 export default createSession;
